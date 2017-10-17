78b3f408ac633013bd2b71e751864b455cc69154
TÖRÖK Attila <torokati44@gmail.com>
2017-10-17 16:25:19 +0200

    renames and stuff

--- tests/fingerprint/runWirelessTests.sh
+++ tests/fingerprint/runWirelessTests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./fingerprints *.csv -m wireless -m Wireless -m adhoc -m mobileipv6 -m dymo -m manetrouting -m examples/aodv -m neighborcache -m objectcache -m geometry $*
+./fingerprints.py *.csv -m wireless -m Wireless -m adhoc -m mobileipv6 -m dymo -m manetrouting -m examples/aodv -m neighborcache -m objectcache -m geometry $*
